901ec3b9395a3446145fbc1d70329e22b7796be2
Beytullah Atasoy <beytullahatasoy55@gmail.com>
2026-01-19 01:16:41 +0300

    ui section

--- Ui_Section/App.tsx
+++ Ui_Section/App.tsx
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, Pressable, TouchableOpacity, ScrollView, ActivityIndicator} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 export default function App() {
+
+  const mesajGoster = () => {
+    alert("Butona tıklandı!");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.baslik}>Selamlar</Text>
-      <Text style={styles.altbaslik}>Bu bir test metnidir.</Text>
-      <Text style={styles.anametin}>Şuan da </Text>
-      <Text style={styles.anametin}>
-        <Text style={styles.koyu}>React Native</Text> ile uygulama geliştiriyorum.
-      </Text>
-      <StatusBar style="auto" />
+      <ScrollView showsVerticalScrollIndicator={false} horizontal={false}>
+        <Image source={require('./assets/icon.png')} style={styles.img} />
+        <Image source={{uri: 'https://cdn.pixabay.com/photo/2025/12/28/13/04/giant-panda-10039235_1280.jpg'}} style={styles.img} />
+        <StatusBar style="auto" />
+        <Text style={styles.text}>Selamlar</Text>
+
+        <Button title= "Tıkla" onPress={mesajGoster} />
+        <Button title= "Tıkla" onPress={mesajGoster} disabled/>
+
+        <Pressable onPress={() => console.log("Buton tıklandı!")}> 
+          <Text style={styles.text}>Test</Text>
+        </Pressable>
+
+        <Pressable onPress={() => console.log("Resim buton tıklandı!")}> 
+          <Image source={require('./assets/icon.png')} style={styles.img} />
+        </Pressable>
+
+        <Pressable onPress={() => console.log("Animasyonlu Resim buton tıklandı!")}> 
+          <Image source={require('./assets/icon.png')} style={styles.img} />
+        </Pressable>
+
+        <ActivityIndicator />
+        <ActivityIndicator size={"large"}/>
+        <ActivityIndicator size={"large"} color={"red"}/>
+
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -22,28 +45,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#646464',
 
   },
-  baslik: {
+  img: {
+    width: 200,
+    height: 200
+  },
+  text: {
     fontSize: 30,
-    fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 20,
-    color: '#9d3131',
+    color: '#ffffff'
   },
-  altbaslik: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginTop: 10,
-    color: '#73aa73',
-  },
-  anametin: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 10,
-    color: '#fdf9f9',
-  },
-  koyu: {
-    fontWeight: 'bold',
-    color: '#693ac6',
-    textDecorationLine: 'underline',
-  },   
+  button: {
+    marginTop: 20,
+    backgroundColor: '#ff0000',
+    color: '#00ff00'
+  }
 });
