ae4ba7b04a82ff1ced672ee0d25dd761a816e564
Beytullah Atasoy <beytullahatasoy55@gmail.com>
2026-01-22 23:15:15 +0300

    mini profile app

--- 7.mini_PROFILE_APP/App.tsx
+++ 7.mini_PROFILE_APP/App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { s } from "react-native-size-matters";
+import ProfileCard from "./components/ProfileCard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("./assets/indir2.avif")}
+        style={styles.bg}
+      >
+        <Text style={styles.title}>Mini Profile App</Text>
+
+        <ProfileCard />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bg: {
+    flex: 1,
+    padding: s(58),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: s(32),
+    fontWeight: "bold",
+    color: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
